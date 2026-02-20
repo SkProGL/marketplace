f4f0c6ca7ab432944048cda28d97438ec2d86815
kilmorosa <ksenija2.kilmasova@live.uwe.ac.uk>
2026-02-20 16:03:18 +0000

    add login page

--- DESD_COOP/DESD/views.py
+++ DESD_COOP/DESD/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import LoginForm
 # Create your views here.
+
+def home_view(request):
+    return render(request, 'home.html') # A simple welcome page
+def login_view(request):
+    form = LoginForm()
+    return render(request, 'login.html', {'form': form})
