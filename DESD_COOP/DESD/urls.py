e8f286148091b3001630aec14fa4fd695b1e7887
kilmorosa <ksenija2.kilmasova@live.uwe.ac.uk>
2026-02-25 13:38:42 +0000

    merge with main

--- DESD_COOP/DESD/urls.py
+++
@@ -1,8 +0,0 @@
-# app/urls.py
-from django.urls import path
-from DESD import views
-
-urlpatterns = [
-    path('', views.home_view, name='home'),
-    path('login/', views.login_view, name='login'), 
-]
