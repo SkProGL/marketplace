cf687f20d16a643ef191e97207fbab47f7565b5c
kilmorosa <ksenija2.kilmasova@live.uwe.ac.uk>
2026-02-25 13:38:42 +0000

    merge with main

--- marketplace_platform/core/urls.py
+++ marketplace_platform/core/urls.py
@@ -1,6 +1,7 @@
 # app/urls.py
-from django.urls import path
-from DESD import views
+from django.contrib import admin
+from django.urls import path, include
+from core import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
