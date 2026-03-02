838579cb2df32677aab30ec220b7758936f86c1e
kilmorosa <ksenija2.kilmasova@live.uwe.ac.uk>
2026-03-02 13:52:16 +0000

    add inventory mvp

--- marketplace_platform/marketplace_platform/settings.py
+++ marketplace_platform/marketplace_platform/settings.py
@@ -22,6 +22,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'core.User'
 LOGOUT_REDIRECT_URL = 'home'
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
