104cb5b4f40f3d1462ef007d3e89022f3194fa38
kilmorosa <ksenija2.kilmasova@live.uwe.ac.uk>
2026-03-03 20:45:17 +0000

    adapt inventory upload to use new models

--- marketplace_platform/core/forms.py
+++ marketplace_platform/core/forms.py
@@ -1,12 +1,31 @@
 from django import forms
-from .models import Item
+from .models import Product
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
-
-class ItemForm(forms.ModelForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+    )
+class ProductForm(forms.ModelForm):
     class Meta:
-        model = Item
-        fields = ['name', 'category', 'short_description', 'price', 'allergens', 'image']
+        model = Product
+        fields = [
+            'name', 
+            'category', 
+            'description', 
+            'price', 
+            'unit', 
+            'season', 
+            'food_miles', 
+            'stock', 
+            'allergens', 
+            'organic', 
+            'surplus', 
+            'image'
+        ]
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 3}),
+        }
