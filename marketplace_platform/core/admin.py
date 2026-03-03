9374890596f0e36658ed87841d1f4bc80a32f7b5
Ironcloud72 <jaimespencer2@hotmail.co.uk>
2026-03-03 13:35:18 +0000

    register all models for admin view

--- marketplace_platform/core/admin.py
+++ marketplace_platform/core/admin.py
@@ -1,6 +1,12 @@
+from django.apps import apps
 from django.contrib import admin
 
-# Register your models here.
-from .models import Product
+# Register all models
+app_models = apps.get_app_config('core').get_models()
 
-admin.site.register(Product)
+for model in app_models:
+    try:
+        admin.site.register(model)
+    except Exception as e:
+        print("Error registering:", e)
+        pass
