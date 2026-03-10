4e5a00a61bd9d03f6a898a499bf34a134abc3768
Zaki Guled <zakiguled@gmail.com>
2026-03-10 00:29:27 +0000

    hotfix: table rename

--- marketplace_platform/core/models.py
+++ marketplace_platform/core/models.py
@@ -126,7 +126,7 @@ class Recipe(models.Model):
     season=models.CharField(max_length=20,choices=Season.choices,default=Season.SPRING)
     ingredients=models.ManyToManyField(Product,through="RecipeProducts")
 
-class RecipeProducts(models.Model):
+class RecipeIngredients(models.Model):
     recipe=models.ForeignKey(Recipe, on_delete=models.CASCADE)
     product=models.ForeignKey(Product,on_delete=models.CASCADE)
     class Meta:
