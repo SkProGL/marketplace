f7ae87044d699802038517334b016775d4737325
Zaki Guled <zakiguled@gmail.com>
2026-03-10 00:12:11 +0000

    hotfix: removing redundant field Order.numPurchased

--- marketplace_platform/core/models.py
+++ marketplace_platform/core/models.py
@@ -84,7 +84,6 @@ class Order(models.Model):
         Product,
         through="OrderProduct"
     )
-    num_purchased=models.IntegerField()
     total_price=models.DecimalField(max_digits=10,decimal_places=2)
     order_date=models.DateTimeField(auto_now_add=True)
     delivery_date=models.DateTimeField()
@@ -94,7 +93,7 @@ class Order(models.Model):
 class OrderProduct(models.Model):
     order=models.ForeignKey(Order, on_delete=models.CASCADE)
     product=models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity=models.IntegerField()
+    numPurchased=models.IntegerField()
     class Meta:
         unique_together=("order","product")
 
