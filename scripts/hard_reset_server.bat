8b70bff41a5f6c7cba246eebba10614f82cda9d4
Ironcloud72 <jaimespencer2@hotmail.co.uk>
2026-03-18 19:11:32 +0000

    Add scripts directory and create bash equivalent for reset

---
+++ scripts/hard_reset_server.bat
@@ -0,0 +1,20 @@
+@echo off
+for /r %%i in (migrations\*.py) do (
+    if not "%%~nxi"=="__init__.py" del "%%i"
+)
+
+@REM Remove containers and delete volumes
+docker compose down -v
+
+@REM Rebuild images
+docker compose build
+
+@REM Create superuser and make migrations
+docker compose run ^
+  -e DJANGO_SUPERUSER_USERNAME=root ^
+  -e DJANGO_SUPERUSER_EMAIL=root@example.com ^
+  -e DJANGO_SUPERUSER_PASSWORD=password123 ^
+  --rm web sh -c "python manage.py makemigrations && python manage.py migrate && python manage.py createsuperuser --noinput"
+
+@REM Start the containers
+docker compose up
---
+++ scripts/restart_migration.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM hard reset
+REM docker compose down -v
+REM docker compose up -d --build
+
+for /r %%i in (migrations\*.py) do (
+    if not "%%~nxi"=="__init__.py" del "%%i"
+)
+
+docker exec django_app python manage.py makemigrations
+docker exec django_app python manage.py migrate
