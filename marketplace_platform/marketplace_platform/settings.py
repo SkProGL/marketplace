98db51fcc3985aa1088593fb6d341bed002d32a4
Ironcloud72 <jaimespencer2@hotmail.co.uk>
2026-03-23 13:14:56 +0000

    Define LOGIN_URL for login decorator redirect. Add producer alerts to context_processers

--- marketplace_platform/marketplace_platform/settings.py
+++ marketplace_platform/marketplace_platform/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'core.User'
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home' # URL for after login
+LOGIN_URL = '/login/' # URL for accessing @login_required functions
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -72,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.get_producer_alerts',
             ],
         },
     },
