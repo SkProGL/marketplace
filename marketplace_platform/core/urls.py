5235fa09a465cf246741c628663bbfdaa3d06bc6
kilmorosa <148281068+kilmorosa@users.noreply.github.com>
2026-05-09 19:07:15 +0100

    Add image upload feedback (#74)
    * add grading feedback
    
    * small icon changes

--- marketplace_platform/core/urls.py
+++ marketplace_platform/core/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('community/recipe/<uuid:recipe_id>/favourite/', views.toggle_favourite_recipe, name='toggle_favourite_recipe'),
     path('community/report/', views.report_content, name='report_content'),
     path('inventory_upload/', views.upload_item, name='inventory_upload'),
+    path('inventory_upload/analyse/', views.analyse_image, name='analyse_image'),
     # Order URLs
     path('orders/', views.orders, name='orders'),
     path('orders/<uuid:order_id>/reorder/', views.reorder, name='reorder'),
