8caadfc54aa51a58bfa35b0b77c983277c05e218
kilmorosa <ksenija2.kilmasova@live.uwe.ac.uk>
2026-05-10 18:29:16 +0100

    restore autologin

--- marketplace_platform/core/middleware.py
+++ marketplace_platform/core/middleware.py
@@ -2,6 +2,25 @@ import time
 
 from django.db import DatabaseError
 from django.utils.deprecation import MiddlewareMixin
+from django.contrib.auth import login
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class AutoLoginMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if settings.DEBUG and not request.user.is_authenticated:
+            superuser = User.objects.filter(is_superuser=True).first()
+            if superuser:
+                login(request, superuser, backend='django.contrib.auth.backends.ModelBackend')
+        
+        response = self.get_response(request)
+        return response
 
 
 # Path prefixes that should never be logged: too noisy or recursive
